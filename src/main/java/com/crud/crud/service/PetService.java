44239cc22e440dd95acf258aed5083cc43acaa38
johnkinuthiaa <kinuthiajohn122@gmail.com>
2024-10-16 19:10:48 +0300

    change styles

--- src/main/java/com/crud/crud/service/PetService.java
+++ src/main/java/com/crud/crud/service/PetService.java
@@ -9,6 +9,8 @@ import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
+import static java.util.Collections.emptyList;
+
 @Service
 public class PetService implements PetServiceInterface{
     private final PetRepository repository;
